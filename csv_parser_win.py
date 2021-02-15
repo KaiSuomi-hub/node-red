ef4e208c0cfe3f5efd38b50991b02da8ab43aa32
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2021-02-15 19:20:42 +0200

    Backend done

--- csv_parser_win.py
+++ csv_parser_win.py
@@ -2,23 +2,23 @@
 
 # .csv Columns
 head = str("Time,temperature,humidity,pressure")
-data = str("/home/pi/Documents/nodered/data.csv")
-# siivoaa data.csnv graafille luettavaan muotoon
+
+# siivoaa data.csvn graafille luettavaan muotoon
 # avataan data
-with open(data, "r") as f:
+with open("data.csv", "r") as f:
     lines = f.readlines()
 
 # kirjoitetaan kaikki rivit paitsi toistuva column names
-with open(data, "w") as f:
+with open("data.csv", "w") as f:
     for line in lines:
         if line.strip("\n") != head:
             f.write(line)
 # avataan lukuna csv ja luetaan temppiin
-f = open(data,'r')
+f = open('data.csv','r')
 temp = f.read()
 f.close()
 # avataan kirjoitettavana, kirjoitetaan columns ja suljetaan
-f = open(data, 'w')
+f = open('data.csv', 'w')
 f.write(head+"\n")
 f.write(temp)
 f.close()
